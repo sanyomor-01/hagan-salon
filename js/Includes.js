61993e3b2b8f37fd0f75d846f7122ad4a1b0c38b
Sanyomor-01 <sanyomormichael@gmail.com>
2025-09-01 19:47:32 +0000

    feat:  about page implemented

--- js/Includes.js
+++ js/Includes.js
@@ -3,11 +3,11 @@ class SpecialHeader extends HTMLElement {
         this.innerHTML = `<header id="header">
             <nav>
                 <div class="logo">
-                <img src="./images/default-monochrome-black 1.svg" alt="logo" />
+                <img src="./images/default-monochrome-black 1.svg" alt="hair" />
                 </div>
                 <ul>
                 <li><a class="link active" href="index.html">Home</a></li>
-                <li><a class="link" href="index.html">Our Story</a></li>
+                <li><a class="link" href="about.html">Our Story</a></li>
                 <li><a class="link" href="index.html">Contact</a></li>
                 </ul>
             </nav>
@@ -63,5 +63,47 @@ class SpecialFooter extends HTMLElement {
     }
 }
 
+
+class Booking extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+              <section id="four">
+      <div class="container text-center">
+        <span class="section-tag booking-tag text-center">Booking</span>
+        <div class="row column text-left gap-md justify-center">
+          <div>
+            <header class="major">
+              <h2 class="white">Book Us Now</h2>
+            </header>
+            <p class="location-name mb-md semi-bold p-1 text-center">
+              Hill Top - Kumasi
+            </p>
+            <p class="line-height">
+              We work by <span class="fw-light">appointment</span> only to give
+              each client the time and attention they deserve. <br />
+              No <span class="line-through">walk-ins</span> walk-ins — please
+              schedule in advance to secure your spot.
+            </p>
+          </div>
+
+          <div>
+            <ul class="flex-column">
+              <li class="contact a-btn text-center">
+                <a class="cta-2" href="">+1 903 033 2343</a>
+              </li>
+              <li class="contact-us a-btn text-center">
+                <a class="cta-2 white" href="">CONTACT US</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+        
+        `
+    }
+}
+
+customElements.define('special-booking', Booking)
 customElements.define('special-header', SpecialHeader)
 customElements.define('special-footer', SpecialFooter)
